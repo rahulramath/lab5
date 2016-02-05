cce0efff2c47f0d126c48a985a3193d13daec5a4
Rahul Ramath <rahulramath1@gmail.com>
2016-02-04 17:31:24 -0800

    minor spelling changes and stretch goal added

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) {
+	res.render('add');
+	console.log("his");
+var newFriend = JSON.parse('{ "name": "' +
+	req.query.name+'", "description": "'+
+	req.query.description+'", "imageURL": "'+
+	'http://lorempixel.com/500/500/people"}');
+data["friends"].push(newFriend);
+	// Your code goes hereres.render('add',newFriend);
+
+}
